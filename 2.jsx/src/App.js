fd95116ee24869852f86b9860bf6a03c13825cc6
formegusto <formegusto@gmail.com>
2020-11-21 11:54:40 +0900

    JSX Okay

---
+++ 2.jsx/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+      // <div>
+      //   나는 주석이다
+      // </div>
+      <></>
+  );
+}
+
+export default App;
---
+++ 2.jsx/src/exportEx/exportEx.js
@@ -0,0 +1,9 @@
+const number_1 = 1;
+const number_2 = 2;
+
+export { number_2 };
+    /* 
+        선언과 동시에 내보내기도 가능합니다!
+        export const number_2 = 2;
+    */
+export default number_1;
